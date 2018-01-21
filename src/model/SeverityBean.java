677066ade9645dc57e8881148ce9a14b6bc93c69
9720JAS-PC-DT <9720JAS-PC-DT@192.168.1.8>
2018-01-21 17:52:19 +0800

    New beans added

---
+++ src/model/SeverityBean.java
@@ -0,0 +1,15 @@
+package model;
+
+public class SeverityBean {
+
+	public String severityName;
+
+	public String getSeverityName() {
+		return severityName;
+	}
+
+	public void setSeverityName(String severityName) {
+		this.severityName = severityName;
+	}
+
+}
