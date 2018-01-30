0f8ca2769e20925ae71c8af1cfbe10e72d40c0d8
Mnizus <bayabusm@gmail.com>
2018-01-30 19:49:33 +0800

    Profile

--- web/js/profile.js
+++ web/js/profile.js
@@ -2,14 +2,27 @@ var response;
 $("#profileForm").submit(function(e) {
    e.preventDefault();
 	});
+
+$('document').ready(function(){
+	$.post('LoadProfileServlet', function (response) {
+		$("#username").val(response["Username"])
+		$("#lastName").val(response["Lastname"])
+		$("#firstName").val(response["Firstname"])
+		$("#middleInitial").val(response["MiddleName"])
+	}).fail(function(){
+		});	
+	
+	
+});
 	
 $('#profileForm').submit(function() {
 	var $form = $(this);
-	assignValues();
 	$.post('ProfileProcessServlet', $form.serialize(), function (response) {
-		alert(response);
 		if(response == "Success"){
 			alert(response);
+			$("oldPassword").val("");
+			$("password").val("");
+			$("confirmPassword").val("");
 		}else{
 			alert(response);
 		}
